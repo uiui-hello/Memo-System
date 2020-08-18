99dedd15683bf92c750e43b23c74bfd00aba692b
Cloud User <centos@ip-172-31-29-232.us-east-2.compute.internal>
2020-08-18 16:22:11 +0900

    add: 新規メモ作成ボタン追加

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
 <?php require('dbconnect.php'); ?>
 <!doctype html>
 <html lang="ja">
-
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/style.css">
-
     <title>メモ管理システム(PHP)</title>
+    <link rel="stylesheet" href="style.css" />
 </head>
-
 <body>
     <header>
-        <h1 class="font-weight-normal">メモ管理システム(PHP)</h1>
+        <h1>メモ管理システム(PHP)</h1>
     </header>
-
     <main>
         <h2>メモ一覧</h2>
+        <a class="newmemo" href="input.html">新規メモ作成</a>
         <?php
         if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
             $page = $_REQUEST['page'];
@@ -28,11 +22,11 @@
         }
         $start = 5 * ($page - 1);
         $memos = $db->prepare('SELECT * FROM memos ORDER BY id LIMIT ?,5');
-        $memos->bindParam(1, $_REQUEST['page'], PDO::PARAM_INT);
+        $memos->bindParam(1, $start, PDO::PARAM_INT);
         $memos->execute();
         ?>
         <article>
-            <?php while ($memo = $memos->fetch()) : ?>
+            <?php while ($memo = $memos->fetch()): ?>
                 <p>
                     <a href="memo.php?id=<?php print($memo['id']); ?>">
                         <?php print(mb_substr($memo['memo'], 0, 50)); ?>
@@ -43,7 +37,7 @@
                 <hr>
             <?php endwhile; ?>
 
-            <?php if ($page >= 2) : ?>
+            <?php if ($page >= 2): ?>
                 <a href="index.php?page=<?php print($page - 1); ?>"><?php print($page - 1); ?>ページ目へ</a>
             <?php endif; ?>
             |
@@ -51,7 +45,7 @@
             $counts = $db->query('SELECT COUNT(*) AS cnt FROM memos');
             $count = $counts->fetch();
             $max_page = ceil($count['cnt'] / 5) + 1;
-            if ($page < $max_page) :
+            if ($page < $max_page):
             ?>
                 <a href="index.php?page=<?php print($page + 1); ?>"><?php print($page + 1); ?>ページ目へ</a>
             <?php endif; ?>
--- update.php
+++ update.php
@@ -1,15 +1,12 @@
 <?php require('dbconnect.php'); ?>
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>メモ管理システム(PHP)</title>
 </head>
-
 <body>
-
     <main>
         <h2>メモ管理システム(PHP)</h2>
         <?php
